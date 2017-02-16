ee98027890bb719f9b14b2f0330003708f6dd482
Damián Silvani <munshkr@gmail.com>
2017-02-16 00:55:22 -0300

    Handle method_missing on main object to create Streams (fixes #3 and #4)
    Define method_missing and create a Stream instances whenever an
    undefined variable or method on main is referenced, with that name.
    Include #set parameters as keyword arguments. Use name as :s parameter
    by default.
    
    For comparison:
    
    Old way: ```ruby
    keys = Supercollider::Stream.new(clock)
    keys.set(freq: [440, 660, 770].p(1/4))
    ```
    New way: ```ruby
    keys freq: [440, 660, 770].p(1/4)
    ```

--- lib/xi.rb
+++ lib/xi.rb
@@ -5,22 +5,67 @@ require 'xi/event'
 require 'xi/stream'
 require 'xi/clock'
 
-def inf
-  Float::INFINITY
+module Xi
+  def self.default_backend
+    @default_backend
+  end
+
+  def self.default_backend=(new_name)
+    @default_backend = new_name && new_name.to_sym
+  end
+
+  module Init
+    def peek(pattern, *args)
+      pattern.peek(*args)
+    end
+
+    def peek_events(pattern, *args)
+      pattern.peek_events(*args)
+    end
+
+    def inf
+      Float::INFINITY
+    end
+
+    def clock
+      @default_clock ||= Clock.new
+    end
+
+    def stop_all
+      @streams.each do |backend, ss|
+        ss.each do |name, stream|
+          stream.stop
+        end
+      end
+    end
+    alias_method :hush, :stop_all
+
+    def method_missing(method, backend=nil, **params)
+      backend ||= Xi.default_backend
+
+      @streams ||= {}
+      @streams[backend] ||= {}
+      s = @streams[backend][method] ||= begin
+        cls = if backend
+          require "xi/#{backend}"
+          Class.const_get("#{backend.to_s.capitalize}::Stream")
+        else
+          Stream
+        end
+        cls.new(method, self.clock)
+      end
+      s.set(s: method, **params) unless params.empty?
+      s
+    end
+  end
 end
 
-module Xi::Init
-  def default_clock
-    @default_clock ||= Clock.new
-  end
+singleton_class.include Xi::Init
 
-  def peek(pattern, *args)
-    pattern.peek(*args)
-  end
-
-  def peek_events(pattern, *args)
-    pattern.peek_events(*args)
-  end
+# Try to load Supercollider backend and set it as default if installed
+begin
+  require "xi/supercollider"
+  Xi.default_backend = :supercollider
+rescue LoadError
+  Xi.default_backend = nil
 end
-
-self.extend Xi::Init
