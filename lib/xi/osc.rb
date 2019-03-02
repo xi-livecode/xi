2eab95e4ab2e7b455d1ce6ddfe8fbf57e9744ca2
Damián Silvani <munshkr@gmail.com>
2019-03-01 21:29:43 -0300

    Integrate xi-osc

---
+++ lib/xi/osc.rb
@@ -0,0 +1,36 @@
+require "xi/osc/version"
+require 'osc-ruby'
+
+module Xi
+  module OSC
+    def initialize(name, clock, server: 'localhost', port:, **opts)
+      super
+      @osc = ::OSC::Client.new(server, port)
+    end
+
+    private
+
+    def send_msg(address, *args)
+      msg = message(address, *args)
+      debug(__method__, msg.address, *msg.to_a)
+      send_osc_msg(msg)
+    end
+
+    def send_bundle(address, *args, at: Time.now)
+      msg = message(address, *args)
+      bundle = ::OSC::Bundle.new(at, msg)
+      debug(__method__, msg.address, at.to_i, at.usec, *msg.to_a)
+      send_osc_msg(bundle)
+    end
+
+    def message(address, *args)
+      ::OSC::Message.new(address, *args)
+    end
+
+    def send_osc_msg(msg)
+      @osc.send(msg)
+    rescue StandardError => err
+      error(err)
+    end
+  end
+end
---
+++ lib/xi/osc/version.rb
@@ -0,0 +1,5 @@
+module Xi
+  module OSC
+    VERSION = "0.1.5"
+  end
+end
